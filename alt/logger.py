0e79c54e73552d46a67be369e7bb8cd841c16744
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-11-17 03:21:02 -0500

    logger(chore): Add type annotations

--- alt/logger.py
+++ alt/logger.py
@@ -6,11 +6,16 @@ Automate the default logger instance.
 
 import logging
 import sys
+from logging import Logger
 
 LOGGER_FORMAT = "%(asctime)s - %(filename)s:%(lineno)d - %(levelname)s - %(message)s"
 
 
-def get_default_logger(name=None, level=logging.DEBUG, fmt=LOGGER_FORMAT):
+def get_default_logger(
+    name: str = None,
+    level: int = logging.DEBUG,
+    fmt: str = LOGGER_FORMAT,
+) -> Logger:
     """Get a default logger instance with specified configuration."""
     logger = logging.getLogger(name if name else "alt")
     if not logger.hasHandlers():
