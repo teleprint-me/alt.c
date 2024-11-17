30dbdebd83c581d16ce99ab5605f9c95be6a164a
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-11-17 03:19:39 -0500

    logger(refactor): Allow overriding the formatter

--- alt/logger.py
+++ alt/logger.py
@@ -7,14 +7,15 @@ Automate the default logger instance.
 import logging
 import sys
 
+LOGGER_FORMAT = "%(asctime)s - %(filename)s:%(lineno)d - %(levelname)s - %(message)s"
 
-def get_default_logger(name=None, level=logging.DEBUG):
+
+def get_default_logger(name=None, level=logging.DEBUG, fmt=LOGGER_FORMAT):
     """Get a default logger instance with specified configuration."""
-    LOGGER_FORMAT = "%(asctime)s - %(filename)s:%(lineno)d - %(levelname)s - %(message)s"
-    logger = logging.getLogger(name)
+    logger = logging.getLogger(name if name else "alt")
     if not logger.hasHandlers():
         handler = logging.StreamHandler(stream=sys.stdout)
-        formatter = logging.Formatter(LOGGER_FORMAT)
+        formatter = logging.Formatter(fmt)
         handler.setFormatter(formatter)
         logger.addHandler(handler)
         logger.setLevel(level)
