36b720dd86b71add867c414675861730b0f8f7dc
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-11-18 17:02:36 -0500

    tensors(patch): Add fix for updated API and handle implicit conversion on stdout

--- examples/tensor.c
+++ examples/tensor.c
@@ -11,9 +11,11 @@ int main() {
     Tensor* tensor = tensor_create(shape, 2);
 
     unsigned int indices[2] = {1, 1};
-    tensor_set(tensor, indices, 3.14);
+    tensor_set_element(tensor, indices, 3.14);
 
-    printf("Value at (1, 1): %f\n", tensor_get(tensor, indices));
+    // @note Handle implicit conversion from float to double.
+    float element = tensor_get_element(tensor, indices);
+    printf("Value at (1, 1): %f\n", (double) element);
 
     tensor_free(tensor);
     return 0;
