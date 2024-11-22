5d8711ab750684bd89d84eb19b657c68b34cecd1
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-11-22 03:04:37 -0500

    activation(patch): Add missing constants, centralize inclusions, and update gelu parameters

--- include/activation.h
+++ include/activation.h
@@ -7,8 +7,23 @@
 #ifndef ALT_ACTIVATION_H
 #define ALT_ACTIVATION_H
 
+#include <math.h>
+#include <stdbool.h>
+
 #ifndef M_PI
-#    define M_PI 3.14159265358979323846
+    #define M_PI 3.14159265358979323846
+#endif
+
+#ifndef PI
+    #define PI 3.141592653589793
+#endif
+
+#ifndef SQRT_2_PI
+    #define SQRT_2_PI 0.7978845608028654 // sqrt(2 / pi)
+#endif
+
+#ifndef SQRT_2
+    #define SQRT_2 1.4142135623730951 // sqrt(2)
 #endif
 
 // Binary Step Activation Function
@@ -27,6 +42,6 @@ double activate_relu(double x);
 double activate_silu(double x);
 
 // Gaussian Error Linear Unit (GELU)
-double activate_gelu(double x);
+double activate_gelu(double x, bool approximate);
 
 #endif // ALT_ACTIVATION_H
