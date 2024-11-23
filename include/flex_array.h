079e02570e8d6bd6b7fe3c8d08834ee91e1ca79f
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-11-22 20:52:37 -0500

    flex_array(chore): Align object handling across the API and apply formatting

--- include/flex_array.h
+++ include/flex_array.h
@@ -13,25 +13,25 @@
 #include "data_types.h"
 
 typedef enum FlexState {
-    FLEX_ARRAY_SUCCESS,               /**< Operation was successful */
-    FLEX_ARRAY_ERROR,                 /**< Generic error */
-    FLEX_ARRAY_RESIZE,                /**< Resize operation performed */
-    FLEX_ARRAY_OUT_OF_BOUNDS,         /**< Index out of bounds */
+    FLEX_ARRAY_SUCCESS, /**< Operation was successful */
+    FLEX_ARRAY_ERROR, /**< Generic error */
+    FLEX_ARRAY_RESIZE, /**< Resize operation performed */
+    FLEX_ARRAY_OUT_OF_BOUNDS, /**< Index out of bounds */
     FLEX_ARRAY_MEMORY_ALLOCATION_FAILED /**< Memory allocation failure */
 } FlexState;
 
 typedef struct FlexArray {
     // Member variables
-    void* data;              /**< Pointer to the array's data */
+    void* data; /**< Pointer to the array's data */
     unsigned int element_size; /**< Size of each element in bytes */
-    unsigned int length;      /**< Current number of elements */
-    unsigned int capacity;    /**< Total allocated capacity */
-    DataType type;            /**< Data type of the elements */
+    unsigned int length; /**< Current number of elements */
+    unsigned int capacity; /**< Total allocated capacity */
+    DataType type; /**< Data type of the elements */
 
     // Function pointers for methods
-    FlexState (*append)(struct FlexArray*, void* element);  /**< Append an element */
-    void* (*pop)(struct FlexArray*);     /**< Pop the last element */
-    void* (*get)(struct FlexArray*, unsigned int index); /**< Get an element by index */
+    FlexState (*append)(struct FlexArray*, void* element); /**< Append an element */
+    FlexState (*pop)(struct FlexArray*, void* element); /**< Pop the last element */
+    FlexState (*get)(struct FlexArray*, unsigned int index, void* element); /**< Get an element by index */
     FlexState (*set)(struct FlexArray*, unsigned int index, void* element); /**< Set an element */
 } FlexArray;
 
@@ -44,8 +44,8 @@ FlexState flex_array_append(FlexArray* array, void* element);
 FlexState flex_array_resize(FlexArray* array, unsigned int new_capacity);
 
 // Accessors
-void* flex_array_get(FlexArray* array, unsigned int index);
+FlexState flex_array_get(FlexArray* array, unsigned int index, void* element);
 FlexState flex_array_set(FlexArray* array, unsigned int index, void* element);
-void* flex_array_pop(FlexArray* array);
+FlexState flex_array_pop(FlexArray* array, void* element);
 
 #endif // ALT_FLEX_ARRAY_H
