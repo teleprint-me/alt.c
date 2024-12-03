88ed831443a83af6400254e301a6f01cc2022006
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-12-02 19:08:05 -0500

    mnist(chore): Add random.h and activation.h and remove old notes for activations

--- examples/models/mnist.c
+++ examples/models/mnist.c
@@ -9,18 +9,17 @@
  *    - data/mnist/testing
  *
  * @note Normalized weight initialization for weights is fine.
- *
- * Potential Options are:
- *    - He: Var(W) = mu + (2 / n)
- *    - Glorot and Bengio: Var(W) = mu + (2 / (n_in + n_out))
  */
 
 #include <dirent.h>
 #include <math.h>
+#include <pthread.h>
 #include <stb/stb_image.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <pthread.h>
+
+#include "activation.h"
+#include "random.h"
 
 int main() {
     srand(42); // Seed for reproducibility
