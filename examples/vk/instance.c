3080a14fb4ef2186c99f9926de0965bff0c1a225
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-12-13 00:31:41 -0500

    vulkan(feat): Add instance example

---
+++ examples/vk/instance.c
@@ -0,0 +1,30 @@
+/**
+ * Copyright © 2024 Austin Berrio
+ *
+ * @file examples/instance.c
+ */
+
+#include "vk/instance.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * @brief Simple example showcasing how to create and destroy a custom VulkanInstance object.
+ */
+int main(void) {
+    const char* appName = "InstanceApp";
+    const char* engineName = "InstanceEngine";
+
+    vulkan_instance_t* vkInstance = vulkan_create_instance(appName, engineName);
+    if (NULL == vkInstance) {
+        fprintf(stderr, "Failed to create Vulkan instance.\n");
+        return EXIT_FAILURE;
+    }
+    printf("Successfully created Vulkan instance!\n");
+
+    vulkan_destroy_instance(vkInstance);
+    printf("Successfully destroyed Vulkan instance!\n");
+
+    return EXIT_SUCCESS;
+}
