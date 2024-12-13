4ce51d75c1d4b0e167472ef4018a64a43c90a30f
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-12-12 21:53:42 -0500

    feat: Add building the library as an option and streamline cleaning up

--- make.sh
+++ make.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+function compile_alt() {
+    cmake -B build -DCMAKE_BUILD_TYPE=Debug
+    cmake --build build --config Debug -j "$(nproc)"
+}
+
 function compile_shaders() {
     for filepath in shaders/*.comp; do
         pathname="$(dirname $filepath)"
@@ -9,6 +14,10 @@ function compile_shaders() {
     done
 }
 
+function cleanup_alt() {
+    rm -rf build build-x64-linux-clang-debug CMakeFiles
+}
+
 function cleanup_shaders() {
     for filepath in shaders/*.spv; do
         rm -v "$filepath"
@@ -16,17 +25,25 @@ function cleanup_shaders() {
 }
 
 function print_usage() {
-    echo "Usage: $0 {shaders | clean | help}"
+    echo "Usage: $0 {all | alt | shaders | clean | help}"
+    echo "  all: Builds the Alt library, examples, and compiles all GLSL shader files."
+    echo "  alt: Builds the Alt library."
     echo "  shaders: Compiles all GLSL shader files in the 'shaders' directory."
     echo "  clean: Removes all compiled shader files in the 'shaders' directory."
     echo "  help: Displays this usage information."
 }
 
 case $1 in
+    all)
+        compile_shaders
+        compile_alt;;
+    alt)
+        compile_alt;;
     shaders)
         compile_shaders;;
     clean)
-        cleanup_shaders;;
+        cleanup_shaders
+        cleanup_alt;;
     help|-h|--help)
         print_usage;;
     *)
