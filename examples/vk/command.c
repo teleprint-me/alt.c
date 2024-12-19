45be538a03bdbc77df4462883d254bdef3275012
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-12-18 22:01:42 -0500

    command(feat): Add example for sketching out command buffer and pool implementation

---
+++ examples/vk/command.c
@@ -0,0 +1,125 @@
+/**
+ * Copyright © 2024 Austin Berrio
+ *
+ * @file examples/vk/command.c
+ *
+ * @brief A simple example showcasing how to implement command buffers and pools
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "logger.h"
+#include "vk/device.h"
+#include "vk/instance.h"
+
+// Define validation layers
+#define VALIDATION_LAYER_COUNT 1 /// @warning Cannot be a variable
+const char* validationLayers[VALIDATION_LAYER_COUNT] = {"VK_LAYER_KHRONOS_validation"};
+
+VkCommandPoolCreateInfo vulkan_create_command_pool_info(uint32_t queueFamilyIndex) {
+    VkCommandPoolCreateInfo commandPoolInfo = {0};
+    commandPoolInfo.sType = VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO;
+    commandPoolInfo.queueFamilyIndex = queueFamilyIndex;
+    commandPoolInfo.flags = VK_COMMAND_POOL_CREATE_RESET_COMMAND_BUFFER_BIT;
+    return commandPoolInfo;
+}
+
+VkCommandBufferAllocateInfo vulkan_create_command_buffer_alloc_info(VkCommandPool commandPool, uint32_t commandBufferCount) {
+    VkCommandBufferAllocateInfo commandBufferAllocInfo = {0};
+    commandBufferAllocInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
+    commandBufferAllocInfo.commandPool = commandPool;
+    commandBufferAllocInfo.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
+    commandBufferAllocInfo.commandBufferCount = commandBufferCount;
+    return commandBufferAllocInfo;
+}
+
+int main(void) {
+    /**
+     * Initialization
+     *
+     * @brief Before using Vulkan, an application must initialize it by loading the Vulkan commands,
+     * and creating a VkInstance object.
+     *
+     * @ref https://docs.vulkan.org/spec/latest/chapters/initialization.html
+     */
+
+    const char* applicationName = "PoolApp";
+    const char* engineName = "PoolEngine";
+
+    VkResult result;
+    VkApplicationInfo applicationInfo = vulkan_create_application_info(applicationName, engineName);
+    VkInstanceCreateInfo instanceInfo = vulkan_create_instance_info(&applicationInfo);
+    vulkan_print_application_info(&applicationInfo); // Output application information
+    result = vulkan_set_instance_info_validation_layers(
+        &instanceInfo, validationLayers, VALIDATION_LAYER_COUNT
+    );
+    if (VK_SUCCESS != result) {
+        return result;
+    }
+
+    VkInstance instance;
+    result = vkCreateInstance(&instanceInfo, NULL, &instance);
+    if (VK_SUCCESS != result) {
+        LOG_ERROR("%s: Failed to create Vulkan instance! (Error code: %d)\n", __func__, result);
+        return result;
+    }
+
+    /**
+     * Devices and Queues
+     *
+     * @brief Once Vulkan is initialized, devices and queues are the primary objects used to
+     * interact with a Vulkan implementation.
+     *
+     * @ref https://docs.vulkan.org/spec/latest/chapters/devsandqueues.html
+     */
+
+    /** Create a physical device */
+    VkPhysicalDevice physicalDevice = vulkan_create_physical_device(instance);
+    uint32_t queueFamilyIndex = vulkan_get_compute_queue_family_index(instance, physicalDevice);
+    VkDeviceQueueCreateInfo deviceQueueInfo = vulkan_create_device_queue_info(queueFamilyIndex, 1);
+    VkDeviceCreateInfo deviceInfo = vulkan_create_device_info(deviceQueueInfo);
+
+    // Create the logical device
+    VkDevice logicalDevice;
+    result = vkCreateDevice(physicalDevice, &deviceInfo, NULL, &logicalDevice);
+    if (VK_SUCCESS != result) {
+        LOG_ERROR("%s: Failed to create logical device! (Error code: %d)\n", __func__, result);
+        vkDestroyInstance(instance, NULL);
+        return result;
+    }
+
+    // For submitting commands to execute compute pipeline.
+    VkQueue computeQueue;
+    vkGetDeviceQueue(logicalDevice, queueFamilyIndex, 0, &computeQueue);
+
+    /**
+     * Command Buffers
+     *
+     * @brief Command buffers are objects used to record commands which can be subsequently
+     * submitted to a device queue for execution.
+     *
+     * @ref https://docs.vulkan.org/spec/latest/chapters/cmdbuffers.html
+     */
+
+    VkCommandPoolCreateInfo commandPoolInfo = vulkan_create_command_pool_info(queueFamilyIndex);    
+
+    VkCommandPool commandPool;
+    if (vkCreateCommandPool(logicalDevice, &commandPoolInfo, NULL, &commandPool) != VK_SUCCESS) {
+        fprintf(stderr, "Failed to create command pool.\n");
+        return EXIT_FAILURE;
+    }
+    VkCommandBufferAllocateInfo allocInfo = vulkan_create_command_buffer_alloc_info(commandPool, 1);
+
+    VkCommandBuffer commandBuffer;
+    if (vkAllocateCommandBuffers(logicalDevice, &allocInfo, &commandBuffer) != VK_SUCCESS) {
+        fprintf(stderr, "Failed to allocate command buffer.\n");
+        return EXIT_FAILURE;
+    }
+
+    // Cleanup
+    vkDestroyDevice(logicalDevice, NULL);
+    vkDestroyInstance(instance, NULL);
+
+    return EXIT_SUCCESS;
+}
