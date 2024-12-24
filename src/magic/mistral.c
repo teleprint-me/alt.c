36e0a775d17b53a0780bd431a227808aaa4caf8f
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-12-23 21:50:18 -0500

    alt(feat): Add initial stub for mistral models

---
+++ src/magic/mistral.c
@@ -0,0 +1,14 @@
+/**
+ * Copyright © 2024 Austin Berrio
+ *
+ * @file src/model/mistral.c
+ *
+ * See specification for more details about the Altiera model file format.
+ *
+ * @ref docs/model/specification.md
+ */
+
+#include <stdbool.h>
+
+#include "model/file.h"
+#include "model/mistral.h"
