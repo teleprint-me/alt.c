6ff68054e73abeadf0984aab2f3eafffbc8ec0d4
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-12-25 00:51:47 -0500

    mistral(feat): Add initial stub for enabling inference

---
+++ examples/models/mistral.c
@@ -0,0 +1,17 @@
+/**
+ * @file examples/models/mistral.c
+ *
+ * @brief Example program showcasing how the magic model file interface operates.
+ *
+ * @note This example only loads the tokenizer and excludes the weights for the mistral model.
+ *
+ * @todo Add implementation for handling weights and biases.
+ */
+
+#include "logger.h"
+#include "model/magic.h"
+#include "model/mistral.h"
+
+int main(void) {
+    return 0;
+}
