90ff3c38744eec9cd089a217f31f2ffd67cd53f0
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2025-01-01 18:38:35 -0500

    alt(feat): Add initial sketch for implementing BPE tokenizer models

---
+++ include/model/tokenizer.h
@@ -0,0 +1,130 @@
+/**
+ * Copyright © 2024 Austin Berrio
+ *
+ * @file include/model/tokenizer.h
+ * @brief Interface for string and token manipulation supporting common ASCII and UTF-8 operations.
+ * @note The tokenizer types are implementation specific. This API simply provides them. Model
+ * implementation details are left up to the user. The models are converted from SentencePiece
+ * binary format to ALT binary format. The tokenizer types here follow the expected file format
+ * according to the specification.
+ * @warning The implementation should expect BPE or BPE-like models and does not support unigram.
+ *
+ * This module is responsible for converting and handling user inputs and outputs.
+ * - Normalization (NFKC Unicode)
+ * - Pre-tokenization
+ * - Merge pairs
+ * - Merge frequencies
+ * - Substitutions
+ * - Post-processing
+ * - Training
+ */
+
+#ifndef ALT_TOKENIZER_H
+#define ALT_TOKENIZER_H
+
+// must be defined before including pcre2.h
+#define PCRE2_CODE_UNIT_WIDTH 8
+#include <pcre2.h>
+
+// Standard C libraries
+#include <string.h>
+
+// Algorithms
+#include "algorithm/hash.h"
+
+// Interfaces
+#include "interface/flex_string.h"
+
+// Googles meta marker representing a space
+#define TOKEN_META_MARKER "\u2581" // UTF-8 marker '▁'
+
+// OpenAI's pre tokenizer perl5 compatible pattern. GPT is good enough for most cases.
+#define GPT_PRE_TOKENIZER_REGEX \
+    "('s|'t|'re|'ve|'m|'ll|'d| ?\\p{L}+| ?\\p{N}+| ?[^\\s\\p{L}\\p{N}]+|\\s+(?!\\S)|\\s+)"
+
+// Add more pre tokenization patterns as needed.
+
+// MagicFile file structures
+/// @note These are model independent, but are designed to mirror the SentencePiece Processor to
+/// enable compatibility with state-of-the-art methods. For these reasons, the implementations are
+/// left up to the end user.
+
+typedef enum TokenType {
+    TOKEN_NORMAL = 0,
+    TOKEN_BYTE = 1,
+    TOKEN_CONTROL = 2,
+    TOKEN_UNKNOWN = 3,
+    TOKEN_UNUSED = 4,
+    TOKEN_BOS = 5,
+    TOKEN_EOS = 6,
+    TOKEN_PAD = 7,
+} TokenType;
+
+typedef struct __attribute__((aligned(8))) Token {
+    float score; // Log-probability of the token data. Use exp(score) to get actual frequency.
+    int32_t type; // Type of the token (e.g., NORMAL, BOS, EOS)
+    int32_t id; // Encoded ID representing the position (e.g. 0, 1, 2)
+    int32_t length; // Length of the token string
+    char* data; // UTF-8 encoded string (dynamically allocated)
+} Token;
+
+typedef struct __attribute__((aligned(8))) TokenizerModel {
+    int32_t vocab_size; // Total number of tokens
+    int32_t bos_id; // Beginning-of-sequence token ID
+    int32_t eos_id; // End-of-sequence token ID
+    int32_t pad_id; // Padding token ID
+    int32_t unk_id; // Unknown token ID
+    Token** tokens; // Array of tokens, indexed by ID
+    HashTable* table; // Hash map for string-based lookups
+} TokenizerModel;
+
+// Byte-pair Encoding
+
+typedef struct VocabularyEntry {
+    char* word; // Space-separated symbols
+    int* frequency; // Pointer to frequency count
+} VocabularyEntry;
+
+VocabularyEntry* create_vocab_entry(const char* word, int frequency);
+void free_vocab_entry(VocabularyEntry* entry);
+
+HashTable* get_stats(HashTable* vocab);
+void free_stats(HashTable* stats);
+
+void merge_vocab(HashTable* vocab, const char* pair);
+
+// The TOKEN_BYTE flag can be used to determine if the token is a byte representation or not
+TokenType get_token_type(Token* token);
+
+// Map byte token to integers
+
+/**
+ * @brief Initializes a hash table for token-to-byte mapping.
+ *
+ * @return A pointer to the initialized hash table.
+ */
+HashTable* create_byte_map(void);
+
+/**
+ * @brief Frees a hash table from token-to-byte mapping.
+ */
+void free_byte_map(HashTable* byte_map);
+
+/**
+ * @brief Converts a byte value to a token string representation.
+ *
+ * @param byte The byte value to convert.
+ * @return A dynamically allocated string representing the byte in hex format (e.g., "<0x01>").
+ */
+char* byte_to_token(unsigned char byte);
+
+/**
+ * @brief Converts a token string back to its corresponding byte value.
+ *
+ * @param map The hash table containing the token-to-byte mappings.
+ * @param token The token string to search for.
+ * @return The byte corresponding to the token, or -1 if not found.
+ */
+int token_to_byte(HashTable* byte_map, const char* token);
+
+#endif // ALT_TOKENIZER_H
---
+++ src/model/tokenizer.c
@@ -0,0 +1,205 @@
+/**
+ * Copyright © 2024 Austin Berrio
+ *
+ * @file include/model/tokenizer.h
+ * @brief Interface for string and token manipulation supporting common ASCII and UTF-8 operations.
+ * @note The tokenizer types are implementation specific. This API simply provides them. Model
+ * implementation details are left up to the user. The models are converted from SentencePiece
+ * binary format to ALT binary format. The tokenizer types here follow the expected file format
+ * according to the specification.
+ * @warning The implementation should expect BPE or BPE-like models and does not support unigram.
+ *
+ * This module is responsible for converting and handling user inputs and outputs.
+ * - Normalization (NFKC Unicode)
+ * - Pre-tokenization
+ * - Merge pairs
+ * - Merge frequencies
+ * - Substitutions
+ * - Post-processing
+ * - Training
+ */
+
+#include "interface/logger.h"
+
+#include "model/tokenizer.h"
+
+VocabularyEntry* create_vocab_entry(const char* word, int frequency) {
+    VocabularyEntry* entry = (VocabularyEntry*) malloc(sizeof(VocabularyEntry));
+    if (!entry) {
+        LOG_ERROR("%s: Failed to allocate memory for VocabularyEntry.\n");
+        return NULL;
+    }
+
+    // Allocate and copy the word
+    entry->word = strdup(word);
+    if (!entry->word) {
+        LOG_ERROR("%s: Failed to allocate memory for word.\n");
+        free(entry);
+        return NULL;
+    }
+
+    // Allocate and initialize frequency
+    entry->frequency = (int*) malloc(sizeof(int));
+    if (!entry->frequency) {
+        LOG_ERROR("%s: Failed to allocate memory for frequency.\n");
+        free(entry->word);
+        free(entry);
+        return NULL;
+    }
+    *(entry->frequency) = frequency;
+
+    return entry;
+}
+
+void free_vocab_entry(VocabularyEntry* entry) {
+    if (entry) {
+        if (entry->word) {
+            free(entry->word);
+        }
+        if (entry->frequency) {
+            free(entry->frequency);
+        }
+        free(entry);
+    }
+}
+
+HashTable* get_stats(HashTable* vocab) {
+    HashTable* stats = hash_create_table(64, HASH_TYPE_STRING);
+
+    for (uint64_t i = 0; i < vocab->size; ++i) {
+        HashEntry* entry = &vocab->entries[i];
+        if (entry->key) {
+            VocabularyEntry* vocab_entry = (VocabularyEntry*) entry->value;
+
+            // Split the word into symbols
+            FlexString* split = flex_string_create_split(vocab_entry->word, " ");
+            if (!split || split->length < 2) {
+                flex_string_free(split); // No pairs possible, free and continue
+                continue;
+            }
+
+            // Create pairs of adjacent symbols
+            for (uint32_t j = 0; j < split->length - 1; ++j) {
+                char pair[64];
+                snprintf(pair, sizeof(pair), "%s %s", split->parts[j], split->parts[j + 1]);
+
+                // Update frequency in stats
+                int* frequency = (int*) hash_search(stats, pair);
+                if (frequency) {
+                    (*frequency) += *(vocab_entry->frequency);
+                } else {
+                    frequency = (int*) malloc(sizeof(int));
+                    *frequency = *(vocab_entry->frequency);
+                    hash_insert(stats, strdup(pair), frequency);
+                }
+            }
+
+            flex_string_free(split);
+        }
+    }
+
+    return stats;
+}
+
+// stats shares pointers with the vocab, but allocates keys.
+void free_stats(HashTable* stats) {
+    if (stats) {
+        // Iterate over the entire hash table size
+        for (uint64_t i = 0; i < stats->size; i++) {
+            HashEntry* entry = &stats->entries[i];
+            if (entry->key) {
+                // Free the key (owned by the caller)
+                free(entry->key);
+                entry->key = NULL;
+            }
+            if (entry->value) {
+                free(entry->value);
+                entry->value = NULL;
+            }
+        }
+
+        // Free the hash table (entries are owned by the hash table)
+        hash_free_table(stats);
+    }
+}
+
+void merge_vocab(HashTable* vocab, const char* pair) {
+    char pattern[64];
+    snprintf(pattern, sizeof(pattern), "(?<!\\S)%s(?!\\S)", pair);
+
+    for (uint64_t i = 0; i < vocab->size; ++i) {
+        HashEntry* entry = &vocab->entries[i];
+        if (entry->key) {
+            VocabularyEntry* vocab_entry = (VocabularyEntry*) entry->value;
+
+            // Replace the pair in the word
+            char* new_word = malloc(strlen(vocab_entry->word) + 1);
+            if (!new_word) {
+                fprintf(stderr, "Memory allocation failed in merge_vocab.\n");
+                exit(EXIT_FAILURE);
+            }
+            strcpy(new_word, vocab_entry->word);
+
+            // Replace pattern with merged token
+            char* pos = strstr(new_word, pair);
+            if (pos) {
+                size_t len = strlen(pair);
+                memmove(pos, pos + len, strlen(pos + len) + 1);
+                memmove(pos, pair, len - 1); // Add merged symbol
+            }
+
+            // Update vocab entry safely
+            char* old_word = vocab_entry->word; // Keep track of the old word
+            free(old_word); // Free the old word AFTER updating the pointer
+            vocab_entry->word = strdup(new_word);
+        }
+    }
+}
+
+HashTable* create_byte_map(void) {
+    // Create a hash table with a size of 256 to map 256 possible byte values
+    HashTable* map = hash_create_table(256, HASH_TYPE_STRING);
+    if (!map) {
+        return NULL; // Handle memory allocation failure
+    }
+
+    // Populate the hash table with the tokens corresponding to each byte value
+    for (unsigned char i = 0; i < 256; ++i) {
+        char* token = byte_to_token(i);
+        if (hash_insert(map, token, (void*) (uintptr_t) i) != HASH_SUCCESS) {
+            free(token); // Handle insertion failure
+            return NULL;
+        }
+        free(token); // Free the temporary token string
+    }
+
+    return map;
+}
+
+char* byte_to_token(unsigned char byte) {
+    // Max value is 0xFF (255 = 2^(n-1))
+    if (byte > 0xFF) {
+        return NULL;
+    }
+
+    // Get the width of a byte token
+    size_t length = strlen("<0xXX>") + 1;
+
+    // Allocate space for the formatted string
+    char* token = (char*) malloc(length * sizeof(char)); // Enough for "<0xXX>" format
+    if (!token) {
+        return NULL; // Handle memory allocation failure
+    }
+
+    // Format the byte as "<0xXX>"
+    snprintf(token, 10, "<0x%02X>", byte);
+    return token;
+}
+
+int token_to_byte(HashTable* byte_map, const char* token) {
+    unsigned char* byte = (unsigned char*) hash_search(byte_map, token);
+    if (!byte) {
+        return -1; // Token not found
+    }
+    return (int) *byte; // Return the byte value
+}
