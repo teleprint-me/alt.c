acaadaa7f23d35b7312cd4d3fc0b79d5f2f5708c
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-12-31 20:43:04 -0500

    flex_string(feat): Add usage example

--- examples/interface/flex_string.c
+++ examples/interface/flex_string.c
@@ -4,6 +4,16 @@
 
 #include "interface/flex_string.h"
 
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MARKER "\xe2\x96\x81" // UTF-8 marker '▁'
+
 int main(void) {
-    return 0;
+    char* source_string = "The quick brown fox jumped over the lazy dog.";
+    char* target_string = flex_string_sub_char_with_uft8(source_string, MARKER, ' ');
+    printf("Source String: %s\n", source_string);
+    printf("Target String: %s\n", target_string);
+    free(target_string);
+    return EXIT_FAILURE;
 }
