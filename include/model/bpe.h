00ad3c47cab6798ae8c2d75c42c7790e63b0412e
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2025-01-11 02:46:52 -0500

    model(feat): Add stubs for BPE modeling

---
+++ include/model/bpe.h
@@ -0,0 +1,14 @@
+/**
+ * @file include/model/bpe.h
+ * @brief Interface of the Byte Pair Encoding (BPE) model.
+ */
+
+#ifndef MODEL_BPE_H
+#define MODEL_BPE_H
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <string.h>
+
+#endif // MODEL_BPE_H
---
+++ src/model/bpe.c
@@ -0,0 +1,7 @@
+/**
+ * @file src/model/bpe.c
+ * @brief Implementation of the Byte Pair Encoding (BPE) model.
+ * @todo Add a test for the BPE model.
+ */
+
+#include "model/bpe.h"
