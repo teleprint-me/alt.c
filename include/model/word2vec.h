dbbd0ce038be96a0608e38e3387c4ef04f5bc2e0
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2025-01-11 02:50:29 -0500

    model(feat): Add stubs for Word2Vec modeling

---
+++ include/model/word2vec.h
@@ -0,0 +1,12 @@
+/**
+ * @file include/model/word2vec.h
+ * @brief Interface for the word2vec model.
+ */
+
+#ifndef ALT_MODEL_WORD2VEC_H
+#define ALT_MODEL_WORD2VEC_H
+
+#include <stdint.h>
+#include <string.h>
+
+#endif // ALT_MODEL_WORD2VEC_H
---
+++ src/model/word2vec.c
@@ -0,0 +1,6 @@
+/**
+ * @file src/model/word2vec.c
+ * @brief Implementation of the Word2Vec model.
+ */
+
+#include "model/word2vec.h"
