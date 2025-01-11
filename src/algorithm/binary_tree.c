fc33025b8426af72637ec0959bf20a62530da5bb
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2025-01-11 03:40:35 -0500

    binary_tree(feat): Add initial rough draft for handling life-cycle functions

--- src/algorithm/binary_tree.c
+++ src/algorithm/binary_tree.c
@@ -15,4 +15,78 @@
  * - Supports in-order, pre-order, and post-order tree walks.
  */
 
-#include <algorithm/binary_tree.h>
+#include "algorithm/binary_tree.h"
+#include "interface/logger.h"
+
+// ---------------------- Life-cycle functions ----------------------
+
+// Create a new pair
+BinaryTreePair* binary_tree_pair_create(void* key, void* value) {
+    BinaryTreePair* pair = malloc(sizeof(BinaryTreePair));
+    if (!pair) {
+        LOG_ERROR("%s: Failed to allocate memory for pair\n", __func__);
+        return NULL;
+    }
+    pair->key = key;
+    pair->value = value;
+    return pair;
+}
+
+// @note This function does not free the key or value.
+// The caller must manage the memory separately.
+void binary_tree_pair_free(BinaryTreePair* pair) {
+    if (pair) {
+        free(pair);
+    }
+}
+
+// Create a new node
+BinaryTreeNode* binary_tree_node_create(BinaryTreePair* pair) {
+    BinaryTreeNode* node = malloc(sizeof(BinaryTreeNode));
+    if (!node) {
+        LOG_ERROR("%s: Failed to allocate memory for node\n", __func__);
+        return NULL;
+    }
+    node->pair = pair;
+    node->left = NULL;
+    node->right = NULL;
+    node->parent = NULL;
+    return node;
+}
+
+void binary_tree_node_free(BinaryTreeNode* node) {
+    if (node) {
+        if (node->pair) {
+            binary_tree_pair_free(node->pair);
+        }
+        free(node);
+    }
+}
+
+// Create a new tree
+BinaryTree* binary_tree_create(void) {
+    BinaryTree* tree = malloc(sizeof(BinaryTree));
+    if (!tree) {
+        LOG_ERROR("%s: Failed to allocate memory for tree\n", __func__);
+        return NULL;
+    }
+    tree->root = NULL;
+    tree->compare = NULL;
+    // DO NOT REINITIALIZE THE RWLOCK!
+    if (0 != pthread_rwlock_init(&tree->rwlock, NULL)) {
+        LOG_ERROR("%s: Failed to initialize rwlock\n", __func__);
+        free(tree);
+        return NULL;
+    }
+    return tree;
+}
+
+void binary_tree_free(BinaryTree* tree) {
+    if (tree) {
+        while (tree->root) {
+            binary_tree_delete(tree, tree->root);
+        }
+        pthread_rwlock_destroy(&tree->rwlock);
+        free(tree);
+    }
+}
