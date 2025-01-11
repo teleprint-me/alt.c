69181ac6adb9f4541d36f58c071f922441ccb8ba
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2025-01-11 03:02:31 -0500

    mlp(feat): Add stubs for multi-layer perceptron

---
+++ include/model/mlp.h
@@ -0,0 +1,12 @@
+/**
+ * @file include/model/mlp.h
+ * @brief Multi-layer perceptron model
+ */
+
+#ifndef ALT_MODEL_MLP_H
+#define ALT_MODEL_MLP_H
+
+#include <pthread.h>
+#include <stdint.h>
+
+#endif // ALT_MODEL_MLP_H
---
+++ src/model/mlp.c
@@ -0,0 +1,6 @@
+/**
+ * @file src/model/mlp.c
+ * @brief Multi-layer perceptron model
+ */
+
+#include "model/mlp.h"
