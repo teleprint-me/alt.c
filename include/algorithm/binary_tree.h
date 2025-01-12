cf0aa778b478da82842fa0a3144448065cfda61f
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2025-01-11 22:54:45 -0500

    binary_tree(patch): Add fixes to interface to align it with the algorithms

--- include/algorithm/binary_tree.h
+++ include/algorithm/binary_tree.h
@@ -19,6 +19,9 @@
 #define BINARY_TREE_H
 
 #include <pthread.h>
+#include <stdlib.h>
+
+// ---------------------- Enumerations ----------------------
 
 typedef enum BinaryTreeState {
     BINARY_TREE_SUCCESS, // Action was successful
@@ -31,14 +34,13 @@ typedef enum BinaryTreeState {
     BINARY_TREE_LOCK_ERROR // Thread lock operation failed
 } BinaryTreeState;
 
+// ---------------------- Structures ----------------------
+
 // Key-value pair for a node in the tree
 typedef struct BinaryTreePair {
     void* key; // The key of the node
     void* value; // The value of the node
 } BinaryTreePair;
-
-// Function pointer to compair pairs
-typedef int (*BinaryTreePairCompare)(const void* key_a, const void* key_b);
 
 // Node in the tree
 typedef struct BinaryTreeNode {
@@ -48,66 +50,88 @@ typedef struct BinaryTreeNode {
     struct BinaryTreeNode* parent;
 } BinaryTreeNode;
 
-// Function pointer to process each node
-typedef void (*BinaryTreeNodeCallback)(BinaryTreeNode* node);
-
 // Tree allows reads, but locks on writes
 typedef struct BinaryTree {
     BinaryTreeNode* root;
-    BinaryTreePairCompare compare;
+    BinaryTreeNodeCompare compare;
     pthread_rwlock_t rwlock; // _POSIX_C_SOURCE requires greater than or equal to 200112L.
 } BinaryTree;
 
+// ---------------------- Function pointers ----------------------
+
+// Function pointer to process each node
+typedef BinaryTreeState (*BinaryTreeNodeCallback)(BinaryTreeNode* node);
+
+/**
+ * @brief Compares two BinaryTreeNode objects.
+ *
+ * @param a Pointer to the first BinaryTreeNode.
+ * @param b Pointer to the second BinaryTreeNode.
+ * @return -1 if a < b, 0 if a == b, 1 if a > b.
+ */
+typedef int (*BinaryTreeNodeCompare)(const BinaryTreeNode* a, const BinaryTreeNode* b);
+
+// ---------------------- Default comparison functions ----------------------
+
+int binary_tree_node_compare_int32(const BinaryTreeNode* a, const BinaryTreeNode* b);
+int binary_tree_node_compare_string(const BinaryTreeNode* a, const BinaryTreeNode* b);
+
 // ---------------------- Life-cycle functions ----------------------
 
-// Create a new pair
+/// @note This function does not allocate the key or value. The caller must manage the memory
+/// separately.
 BinaryTreePair* binary_tree_pair_create(void* key, void* value);
-// @note This function does not free the key or value.
-// The caller must manage the memory separately.
+/// @note This function does not free the key or value. The caller must manage the memory
+/// separately.
 void binary_tree_pair_free(BinaryTreePair* pair);
 
 // Create a new node
 BinaryTreeNode* binary_tree_node_create(BinaryTreePair* pair);
+/// @note This function does not allocate the key or value. The caller must manage the memory
+/// separately.
+BinaryTreeNode* binary_tree_node_create_from_pair(void* key, void* value);
+// Copy a node
+BinaryTreeNode* binary_tree_node_copy(BinaryTreeNode* node);
+// Free a node
 void binary_tree_node_free(BinaryTreeNode* node);
 
 // Create a new tree
-BinaryTree* binary_tree_create(BinaryTreePairCompare compare);
+BinaryTree* binary_tree_create(BinaryTreeNodeCompare compare);
+// Free the tree
 void binary_tree_free(BinaryTree* tree);
-
-// ---------------------- Default comparison functions ----------------------
-
-int binary_tree_pair_int32_compare(const void* key_a, const void* key_b);
-int binary_tree_pair_string_compare(const void* key_a, const void* key_b);
 
 // ---------------------- Insertion and Deletion Functions ----------------------
 
 // Insert a new node into the tree
-BinaryTreeState binary_tree_insert(BinaryTree* tree, BinaryTreePair* pair);
+BinaryTreeState binary_tree_insert(BinaryTree* tree, BinaryTreeNode* node);
 
 // Delete a node from the tree
-BinaryTreeState binary_tree_delete(BinaryTree* tree, BinaryTreePair* pair);
+BinaryTreeState binary_tree_delete(BinaryTree* tree, BinaryTreeNode* node);
 
 // Transplant a node in the tree with a new node
-BinaryTreeState binary_tree_transplant(BinaryTree* tree, BinaryTreeNode* old_node, BinaryTreeNode* new_node);
+BinaryTreeState
+binary_tree_transplant(BinaryTree* tree, BinaryTreeNode* old_node, BinaryTreeNode* new_node);
 
-// ---------------------- Searching Functions ----------------------
+// ---------------------- Search by key ----------------------
 
 // Find a node in the tree
-BinaryTreeNode* binary_tree_find_node(BinaryTree* tree, void* key);
+BinaryTreeNode* binary_tree_search(BinaryTree* tree, void* key);
 
 // Find the minimum key in the tree where x is a node in the tree.
-BinaryTreeNode* binary_tree_find_minimum(BinaryTree* tree, void* key);
+BinaryTreeNode* binary_tree_minimum(BinaryTree* tree, void* key);
 
 // Find the maximum key in the tree where x is a node in the tree.
-BinaryTreeNode* binary_tree_find_maximum(BinaryTree* tree, void* key);
+BinaryTreeNode* binary_tree_maximum(BinaryTree* tree, void* key);
+
+// ---------------------- Search by node ----------------------
 
 // Find the successor of a node in the tree
-BinaryTreeNode* binary_tree_find_successor(BinaryTree* tree, void* key);
+BinaryTreeNode* binary_tree_successor(BinaryTree* tree, BinaryTreeNode* node);
 
 // Find the predecessor of a node in the tree
-BinaryTreeNode* binary_tree_find_predecessor(BinaryTree* tree, void* key);
+BinaryTreeNode* binary_tree_predecessor(BinaryTree* tree, BinaryTreeNode* node);
 
-// ---------------------- Walking the tree ----------------------
+// ---------------------- Walk the tree ----------------------
 
 // Traverse the tree in order
 void binary_tree_inorder_walk(BinaryTree* tree, BinaryTreeNodeCallback callback);
