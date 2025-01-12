a2c011aa694d5d8556be94440ea6c4089c48875a
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2025-01-12 00:09:16 -0500

    key_value_pair(refactor): Add generalize interface
    @note This is just a light rough draft.

--- include/algorithm/key_value_pair.h
+++ include/algorithm/key_value_pair.h
@@ -1,6 +1,8 @@
 /**
  * @file include/algorithm/key_value_pair.h
  * @brief Key-value pair
+ * @warning Thread-safety is not guaranteed. This is mostly due to the flexibility of the data
+ * structure.
  */
 
 #ifndef ALT_KEY_VALUE_PAIR_H
@@ -14,16 +16,24 @@ typedef struct KeyValuePair {
 } KeyValuePair;
 
 typedef struct KeyValuePairAllocator {
-    void* (*allocator)(void* object, size_t size);
-    void (*deallocator)(void* object);
+    // keys
+    void* (*allocate_key)(void* object, size_t size);
+    void (*deallocate_key)(void* object);
+    // values
+    void* (*allocate_value)(void* object, size_t size);
+    void (*deallocate_value)(void* object);
 } KeyValuePairAllocator;
 
+// life-cycle
 KeyValuePair* kv_pair_create(KeyValuePairAllocator* allocator);
-void kv_pair_destroy(KeyValuePair* pair);
+void kv_pair_destroy(KeyValuePairAllocator* allocator, KeyValuePair* pair);
 
+// getters
+void* kv_pair_get_key(const KeyValuePair* pair);
+void* kv_pair_get_value(const KeyValuePair* pair);
+
+// setters
 void kv_pair_set_key(KeyValuePair* pair, void* key);
 void kv_pair_set_value(KeyValuePair* pair, void* value);
-void kv_pair_get_key(KeyValuePair* pair, void** key);
-void kv_pair_get_value(KeyValuePair* pair, void** value);
 
 #endif // ALT_KEY_VALUE_PAIR_H
